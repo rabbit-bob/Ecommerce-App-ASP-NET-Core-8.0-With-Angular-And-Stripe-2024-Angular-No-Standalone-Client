3eb90d0a2940317ef4cdabed9fc4a597b2b227c2
newde <new.dev1975@gmail.com>
2024-08-04 17:37:57 +0200

    Created SharedModule and Refactor AppComponent to use HttpClient with provideHttpClient, implement ngFor in template
    - Updated AppComponent to fetch data using HttpClient
    - Implemented ngFor directive in the template to display products
    - Modified AppModule to use provideHttpClient with interceptors
    - Updated SharedModule to import CommonModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  baseURL = "";
+  products: any[] = [];
+
+  constructor(private http: HttpClient) {}
+  
+  getProduct() {
+    return this.http.get(this.baseURL).subscribe(
+      (values: any) => {
+        this.products = values;
+      }
+    );
+  }
+
+  ngOnInit(): void {
+    this.getProduct();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,12 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(withInterceptorsFromDi())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
