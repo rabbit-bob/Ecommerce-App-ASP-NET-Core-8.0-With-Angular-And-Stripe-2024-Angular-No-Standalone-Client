33f9c5c37333d6141c9a14a1bf867fc07f94c687
newde <new.dev1975@gmail.com>
2024-08-05 18:43:51 +0200

    Integrate product data binding and update UI components
    - Updated shop.component.html to bind products data to ShopItemComponent
    - Modified shop-item.component.html to display product details including image, name, and price
    - Implemented @Input() decorator in ShopItemComponent to accept product data
    - Enhanced ShopItemComponent to display product details dynamically

--- src/app/shop/shop-item/shop-item.component.ts
+++ src/app/shop/shop-item/shop-item.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IProduct } from '../../shared/Models/IProduct';
 
 @Component({
   selector: 'app-shop-item',
   templateUrl: './shop-item.component.html',
   styleUrl: './shop-item.component.scss'
 })
-export class ShopItemComponent {
+export class ShopItemComponent implements OnInit {
+  @Input() product: IProduct | undefined;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 
 }
