77f69542451078d3db000ac967589c74e6e55d46
newde <new.dev1975@gmail.com>
2024-08-05 18:17:39 +0200

    Enhance ShopComponent with ShopItemComponent and update UI
    - Added ShopItemComponent for individual product display in ShopComponent
    - Updated ShopComponent to use ShopItemComponent for product listing
    - Created ShopItemComponent HTML and SCSS for product card layout

---
+++ src/app/shop/shop-item/shop-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-item',
+  templateUrl: './shop-item.component.html',
+  styleUrl: './shop-item.component.scss'
+})
+export class ShopItemComponent {
+
+}
