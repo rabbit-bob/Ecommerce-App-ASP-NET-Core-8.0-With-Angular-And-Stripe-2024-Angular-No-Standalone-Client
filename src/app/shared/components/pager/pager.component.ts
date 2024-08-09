b5bb625261c27856319416ef519646114ccf36a3
newde <new.dev1975@gmail.com>
2024-08-09 14:07:35 +0200

    Feat: Add PagerComponent for pagination and integrate with ShopComponent

---
+++ src/app/shared/components/pager/pager.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-pager',
+  templateUrl: './pager.component.html',
+  styleUrl: './pager.component.scss'
+})
+export class PagerComponent implements OnInit {
+
+  @Input() totalCount!: number;
+  @Input() pageSize!: number;
+  @Output() pageChanged = new EventEmitter<number>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onPagerChanged(event: any) {
+    this.pageChanged.emit(event);
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
+import { PagerComponent } from './components/pager/pager.component';
 
 
 @NgModule({
   declarations: [
-    PagingHeaderComponent
+    PagingHeaderComponent,
+    PagerComponent
   ],
   imports: [
     CommonModule,
@@ -14,7 +16,8 @@ import { PagingHeaderComponent } from './components/paging-header/paging-header.
   ],
   exports: [
     PaginationModule,
-    PagingHeaderComponent
+    PagingHeaderComponent,
+    PagerComponent
   ]
 })
 export class SharedModule { }
