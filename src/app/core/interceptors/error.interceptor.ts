713e0f5eef02cde4a0995f2be2ae883026944212
newde <new.dev1975@gmail.com>
2024-08-10 23:57:08 +0200

    Add Error Handling Components and Update Error Interceptor
    - Created NotFoundComponent and ServerErrorComponent in the core module for handling 404 and 500 errors.
    - Updated AppRoutingModule to include routes for /not-found and /server-error components.
    - Enhanced ErrorInterceptor to navigate to the appropriate error component based on the HTTP status code (404 or 500).

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -1,16 +1,21 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
   return next(req).pipe(
     catchError((err: HttpErrorResponse) => {
       if (err) {
         if (err.status === 404) {
           //
+          router.navigateByUrl('/not-found');
         }
         if (err.status === 500) {
           //
+          router.navigateByUrl('/server-error');
         }
       }
       return throwError(() => new Error(err.message || 'Server Not Found!'));
