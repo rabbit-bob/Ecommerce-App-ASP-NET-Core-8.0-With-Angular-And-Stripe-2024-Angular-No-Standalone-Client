b7407298c6dc3d8eaca59171a0f1e86c8b844d92
newde <new.dev1975@gmail.com>
2024-08-12 23:37:06 +0200

    Add Bootswatch theme and clean up core module
    - Added Bootswatch 'Journal' theme to angular.json for improved styling.
    - Cleaned up CoreModule by removing unnecessary imports.
    - Updated package.json to ensure compatibility with Angular 18.1.4.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -7,7 +7,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { ToastrModule } from 'ngx-toastr';
 import { SectionHeaderComponent } from './section-header/section-header.component';
-import { BreadcrumbComponent, BreadcrumbItemDirective, BreadcrumbService } from 'xng-breadcrumb';
+import { BreadcrumbComponent, BreadcrumbItemDirective } from 'xng-breadcrumb';
 
 
 
