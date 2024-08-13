2e8f04c85ef2f306d877ce7a6145355efdd7aa3f
newde <new.dev1975@gmail.com>
2024-08-13 21:26:04 +0200

    UI Enhancements and Breadcrumb Initialization in Shop and Product Details Components
    - Updated <ngx-spinner> in app.component.html to specify the spinner type as "ball-spin-fade" for a more consistent loading animation.
    - Added *ngIf directives to conditionally render sections in shop.component.html based on the presence of categories and products data.
    - Initialized the breadcrumb in product-details.component.ts by setting @productDetails to an empty string in the constructor.

--- src/app/shop/product-details/product-details.component.ts
+++ src/app/shop/product-details/product-details.component.ts
@@ -15,7 +15,9 @@ export class ProductDetailsComponent implements OnInit {
     private shopService: ShopService,
     private activeRoute: ActivatedRoute,
     private bcService: BreadcrumbService
-  ) {}
+  ) {
+    this.bcService.set('@productDetails', '')
+  }
 
   ngOnInit(): void {
     this.loadProducts();
