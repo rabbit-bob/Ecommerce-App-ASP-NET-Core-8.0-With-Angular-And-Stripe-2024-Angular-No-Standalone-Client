ca0047941f0712e35f3b27bc995586170ed8cf89
newde <new.dev1975@gmail.com>
2024-08-15 23:21:02 +0200

    Title: Implement BasketService with get, set, and current basket methods
    Description:
    - Added BasketService with HttpClient integration.
    - Implemented getBasket method to fetch basket data and update BehaviorSubject.
    - Implemented setBasket method to post basket data and handle response.
    - Added getCurrentBasketValue method to retrieve the current basket value from BehaviorSubject.

--- src/app/basket/basket.service.ts
+++ src/app/basket/basket.service.ts
@@ -1,9 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { BehaviorSubject, map } from 'rxjs';
+import { Basket, IBasket } from '../shared/Models/Basket';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BasketService {
 
-  constructor() { }
+  baseURL: string = environment.baseURL;
+  private basketSource = new BehaviorSubject<IBasket>(new Basket());
+  basket$ = this.basketSource.asObservable();
+  constructor(private http: HttpClient) { }
+
+  getBasket(id: string) {
+    return this.http.get<IBasket>(`${this.baseURL}Baskets/get-basket-item/${id}`)
+      .pipe(
+        map((basket: IBasket) => {
+          this.basketSource.next(basket);
+        })
+      );
+  }
+  
+  setBasket(basket: IBasket) {
+    this.http.post<IBasket>(this.baseURL + 'Baskets/update-basket', basket).subscribe({
+      next: (res: IBasket) => {
+        this.basketSource.next(res);
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }  
+
+  getCurrentBasketValue() {
+    return this.basketSource.value;
+  }
 }
