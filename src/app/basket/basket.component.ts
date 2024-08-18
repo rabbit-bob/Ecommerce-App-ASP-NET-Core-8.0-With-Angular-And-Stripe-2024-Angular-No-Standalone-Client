7b7753a0dad4c378253c585b342b9895bb9255a9
newde <new.dev1975@gmail.com>
2024-08-18 15:12:06 +0200

    Display basket items in BasketComponent and update routing
    - Refactored BasketComponent to utilize BasketService and display basket items through an observable (baskets$).
    - Implemented ngOnInit lifecycle hook to initialize and subscribe to the basket data stream.
    - Enhanced HTML template to conditionally display a message when the basket is empty and list the items when available.
    - Updated BasketRoutingModule to configure BasketComponent as the default route.

--- src/app/basket/basket.component.ts
+++ src/app/basket/basket.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BasketService } from './basket.service';
+import { filter, Observable } from 'rxjs';
+import { IBasket } from '../shared/Models/Basket';
 
 @Component({
   selector: 'app-basket',
   templateUrl: './basket.component.html',
   styleUrl: './basket.component.scss'
 })
-export class BasketComponent {
+export class BasketComponent implements OnInit {
+
+  baskets$!: Observable<IBasket>;
+  constructor(private basketService: BasketService) { }
+
+  ngOnInit(): void {
+    this.baskets$ = this.basketService.basket$.pipe(
+      filter((basket): basket is IBasket => basket !== null)
+    );
+  }
 
 }
