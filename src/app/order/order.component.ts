092094cc11450b0a3db06b18a812319548ed130e
newde <new.dev1975@gmail.com>
2024-11-01 20:10:55 +0100

    feat(order): implement OrderService and OrderComponent for user order management
    Added OrderService with methods to retrieve user orders and order details.
    Updated OrderComponent to display the user’s order list and handle API data.
    Improved template to present orders in a structured table format with router link.

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IOrder } from '../shared/models/order';
+import { OrderService } from './order.service';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrl: './order.component.scss'
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
 
+  orders: IOrder[] = [];
+
+  constructor(private orderService: OrderService) { }
+
+  /**
+   * Initializes the component and retrieves the list of user orders.
+   */
+  ngOnInit(): void {
+    this.getOrders();
+  }
+
+  /**
+   * Calls the order service to fetch user orders and handles the response.
+   * On success, populates the orders array; on error, logs the error message.
+   */
+  getOrders() {
+    this.orderService.getOrdersForUser().subscribe({
+      next: (orders: IOrder[]) => {
+        this.orders = orders;
+      },
+      error: (err) => {
+        console.error(err.message);
+      }
+    });
+  }
+  
 }
+
--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IOrder } from '../shared/models/order';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor() { }
+  baseURL: string = environment.baseURL;
+
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Retrieves the list of orders for the current user.
+   * @returns An observable of the user's orders as an array of IOrder objects.
+   */
+  getOrdersForUser(): Observable<IOrder[]> {
+    return this.http.get<IOrder[]>(`${this.baseURL}Orders/get-orders-for-user`);
+  }
+
+  /**
+   * Fetches the details of a specific order by ID.
+   * @param id The ID of the order to retrieve.
+   * @returns An observable of the specified order as an IOrder object.
+   */
+  getOrderDetails(id: number): Observable<IOrder> {
+    return this.http.get<IOrder>(`${this.baseURL}Orders/get-order-by-id/${id}`);
+  }
+  
 }
