f184f192b747a8b4dc396247e10d2d7ab999bba4
newde <new.dev1975@gmail.com>
2024-12-14 00:12:47 +0100

    feat(ui-interceptor): enhance loading logic and button state handling

--- src/app/core/interceptors/loading.interceptor.ts
+++ src/app/core/interceptors/loading.interceptor.ts
@@ -9,16 +9,21 @@ import { LoadingService } from '../services/loading.service';
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   // Inject LoadingService to control loading indicators
   const loadingService = inject(LoadingService);
-  
+
   // URLs to exclude from showing the loading indicator
-  const skipUrls: string[] = [
-    // '/api/account/login', 
-    // '/api/account/register',
-    '/api/account/check-email-exist'
+  const skipUrls: { url: string; method?: string }[] = [
+    // { url: '/api/account/login', method: 'POST' },
+    // { url: '/api/account/register', method: 'POST' },
+    { url: '/api/Accounts/check-email-exist' },
+    { url: '/api/Orders/create-order', method: 'POST' },
   ];
 
-  // Check if the request URL should be excluded from the loading indicator
-  if (!skipUrls.some((url) => req.url.includes(url))) {
+  // Check if the request URL and method should be excluded from the loading indicator
+  if (
+    !skipUrls.some(
+      (skip) => req.url.includes(skip.url) && (!skip.method || req.method === skip.method)
+    )
+  ) {
     loadingService.startLoading();
   }
 
